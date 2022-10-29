807a00cc1bd4ba67caf94c31eed374bdbdfbdff1
liel berniker <lielb101995@gmail.com>
2022-10-29 18:52:59 +0300

    add all campgrounds al shoe details

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/Campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
 const app = express();
-const path = require('path')
 
 
-
-app.set('view engine', 'ejs')
-
-app.set('views',path.join(__dirname,'views') )
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
 
 
 app.get('/',(req,res)=>{
     res.render('home')
 })
+
+app.get('/campgrounds',async (req,res)=>{
+const campgrounds = await Campground.find({});
+res.render('campgrounds/index',{campgrounds})
+})
+
+app.get('/campgrounds/:id',async (req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{campground})
+    })
 
 app.listen(3000,()=>{
     console.log("serving port 3000")
