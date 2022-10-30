b879fad8319e372dc7d3fab4757ae6422c1f9435
liel berniker <lielb101995@gmail.com>
2022-10-30 20:59:24 +0200

    add redirect and style

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () =>{
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 40)+10
         const camp = new Campground({
            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-           title: `${sample(descriptors)} ${sample(places)}`
+           title: `${sample(descriptors)} ${sample(places)}`,
+           image:"https://source.unsplash.com/collection/483251",
+           description: "    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis rem natus soluta fugit provident, ipsum corrupti consequatur, perspiciatis nesciunt maxime laborum ex minus ut unde molestias odio deserunt doloremque reprehenderit.",
+           price
         })
         await camp.save()
     }
